42e779265f1c4bfe2e694e05890a218df44f4750
Murakami <kazu.3156.lim@icloud.com>
2019-10-01 20:30:26 +0900

    Update Forcescript.cs

--- Assets/script/Forcescript.cs
+++ Assets/script/Forcescript.cs
@@ -6,17 +6,20 @@ public class Forcescript : MonoBehaviour
 {
     Rigidbody rb;//Rigidbodyの宣言
 
-    Vector3 forceway = new Vector3(1.0f, 1.7f, 0f);//射出角度
-    Vector3 windforceway = new Vector3(1.0f, 1.2f, 0);//風力の向き
+    Vector3 forcedirection = new Vector3(1.0f, 1.7f, 0f);//射出角度
+    Vector3 windforcedirection = new Vector3(1.0f, 1.2f, 0);//風力の向き
 
     Vector3 force,windforce;//ロケットに与える力、風力
   //  Vector3 look;        //最高点の方向
-    public Vector3  startposi;
-    public Vector3 endposi;
+    public Vector3  startposition;
+    public Vector3 endposition;
   
     public float injectionforce;//力の値 
-    public float nozzlearea,airpressure,outpressure,water;//ペットボトルの口の面積,空気圧、大気圧、水量
-    public float dis;          //飛んだ距離
+    public float nozzlearea;//ペットボトルの口の面積
+    public float airpressure;//空気圧
+    public float outpressure;//大気圧
+    public float water;//水量
+    public float flyinddistance;          //飛んだ距離
 
     float gravity=9.8f;
     float miri=0.001f;
@@ -34,7 +37,7 @@ public class Forcescript : MonoBehaviour
         waterpressur = water * miri * gravity;//水量から水圧を計算
         target = gameObject;            //ターゲットをこのオブジェクトに
         rb = gameObject.GetComponent<Rigidbody>();//このオブジェクトのRigidbodyを取得
-        startposi = transform.position;           //初期位置をこのオブジェクトの位置に                      
+        startposition = transform.position;           //初期位置をこのオブジェクトの位置に                      
         allpressur = airpressure + waterpressur;   //気圧の合計を算出
        
     }
@@ -43,8 +46,8 @@ public class Forcescript : MonoBehaviour
        
         double num = Mathf.Pow(pre,airratio);
         injectionforce = 7 * nozzlearea * allpressur * ((float)num-1)+nozzlearea*(allpressur*pressurratio-outpressure);//力の値を計算
-        windforce = injectionforce * windforceway;//風力を決定
-        force = injectionforce * forceway;  //発射の力を設定
+        windforce = injectionforce * windforcedirection;//風力を決定
+        force = injectionforce * forcedirection;  //発射の力を設定
 
      //   look = target.transform.position - transform.position;//最高点の座標を算出
     //  Quaternion to = Quaternion.FromToRotation(Vector3.up, look);//ロケットの向きを決定
@@ -56,10 +59,10 @@ public class Forcescript : MonoBehaviour
             Inp();
         }
   
-        if  (startposi.x< transform.position.x)//高さが1より小さく、このオブジェクトの座標が初期座標より大きい時
+        if  (startposition.x< transform.position.x)//高さが1より小さく、このオブジェクトの座標が初期座標より大きい時
         {
-            endposi = transform.position;//到達地点を設定
-            dis = Vector3.Distance(endposi, startposi);
+            endposition = transform.position;//到達地点を設定
+            flyinddistance = Vector3.Distance(endposition, startposition);
            
         }
     }
